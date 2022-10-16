93f0f82727922d7045785019fbe275070dfda752
Rolando <Roland@Roland-MacBook-Pro.local>
2022-10-15 20:30:29 -0600

    feat:sql

--- sql/db_currency.sql
+++ sql/db_currency.sql
@@ -2,7 +2,7 @@ CREATE TABLE db_currency (
     crr_id SERIAL PRIMARY KEY,
     crr_code character varying(255) NOT NULL,
     crr_description character varying(255)
-);
+); 
 
 -- Indices -------------------------------------------------------
 
